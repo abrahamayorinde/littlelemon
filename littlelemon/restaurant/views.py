4245552844f0c4b6b5c23b096be1e6183ad8882a
Abraham Ayorinde <ayorinde.abraham@gmail.com>
2025-02-15 22:15:44 -0500

    Add 'LittleLemonAPI' and token based authentication.

--- littlelemon/restaurant/views.py
+++ littlelemon/restaurant/views.py
@@ -13,6 +13,7 @@ def index(request):
 class MenuItemsView(generics.ListCreateAPIView):
     queryset = Menu.objects.all()
     serializer_class = MenuSerializer
+    permission_classes = [IsAuthenticated]     
 
 class SingleMenuItemView(generics.RetrieveUpdateAPIView, generics.DestroyAPIView):
     queryset = Menu.objects.all()
